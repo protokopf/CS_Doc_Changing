fb8bf01c7d1df3f66399a4fed0907294a44938fb
Justus <protokopf@gmail.com>
2016-07-10 13:47:18 +0300

    Create SetCursAlgorythm!

--- DocFilesFillingProgramm/DocFilesFillingProgrammLogick/Algorythms/ChangeDocumentsAlgorythms/GeneralChangeAlgorythm.cs
+++ DocFilesFillingProgramm/DocFilesFillingProgrammLogick/Algorythms/ChangeDocumentsAlgorythms/GeneralChangeAlgorythm.cs
@@ -10,7 +10,24 @@ namespace DocFilesFillingProgrammLogick.Algorythms.ChangeDocumentsAlgorythms
             document.Open();
             foreach (var fillingPart in info.Fields)
                 document.ReplaceTextInPosition(fillingPart.Value, fillingPart.Key);
+            SetCursAlgorythm(document, info);
             document.Close();
+        }
+
+        private void SetCursAlgorythm(IDocument document, IFillingInfo info)
+        {
+            string curs = (info.Fields["<dn>"] + info.Fields["<dl>"]).ToLower();
+            for(char i = 'a'; i < 'd'; ++i)
+            {
+                for(int j = 1; j < 3; ++j)
+                {
+                    string currentCurs = j.ToString() + i.ToString();
+                    if (currentCurs == curs)
+                        document.ReplaceTextInPosition("X", currentCurs);
+                    else
+                        document.ReplaceTextInPosition(" ", currentCurs);
+                }
+            }
         }
     }
 }
